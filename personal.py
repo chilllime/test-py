f0ef9ee04ba21b527a1dc39ad7973d8e8615a6e4
chilllime <raja.donepalli798@gmail.com>
2024-11-04 18:59:55 +0530

    reg number

---
+++ personal.py
@@ -0,0 +1,11 @@
+#get personal info
+
+name=input('enter the name: ')
+age=int(input('enter your age: '))
+ph=int(input('enter your phone number: '))
+reg=input('enter your reg number: ')
+
+print(name)
+print(age)
+print(ph)
+print(reg)
